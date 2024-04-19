c9a961169ab30285b04b16b7715ad13a1c79a092
KrzysztofRojek <krzysztof-rojek7@wp.pl>
2024-04-19 20:04:55 +0200

    fixing-setup

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => {
-  const config = {
+ const config = {
     plugins: [react()],
-    base: command === 'serve' ? '/' : '/hosting-service-website/',
-  }
+    base: '/hosting-service-website/',
+ }
 
-  if (command !== 'serve') {
-    config.base = '/react-vite-gh-pages/'
-  }
-
-  return config
+ return config
 })
